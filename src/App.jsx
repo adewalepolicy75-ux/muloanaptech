607f94e5018bf1725369d69d3960c12b9333c318
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-04-13 10:30:26 +0100

    others

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css'
+import About from "./components/About";
+
+
+export default function APP() {
+  return (
+    <div className="APP">
+      <h2>Welcome to my portfolio</h2>
+      <About />
+    </div>
+  );
+}
+
