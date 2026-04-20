455fceebc32db27151b0970973890a3ebcd4739c
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-04-20 11:00:14 +0100

    home-page

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,22 @@ import Privacy from "./components/Privacy";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Terms from "./components/Terms";
 import Faq from "./components/Faq";
+import Home from "./pages/Home";
+
+
 export default function APP() {
   return (
     <Router>
       <div className="APP">
       
         <Routes>
+          <Route path="/" element={<Home />} />
           <Route path="/contact" element={<Contact />} />
           <Route path="/about" element={<About />} />
           <Route path="/privacy" element={<Privacy />} />
           <Route path="/terms" element={<Terms />} />
           <Route path="/faq" element={<Faq />} />
+
         </Routes>
         <Footer />
       </div>
