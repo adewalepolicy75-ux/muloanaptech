7aee2fc4a9e591823a19af771f8eda2b35b1d6ff
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-04-29 10:37:32 +0100

    update

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ import Terms from "./components/Terms";
 import Faq from "./components/Faq";
 import Home from "./pages/Home";
 import Signup from "./components/Signup";
-import Login from "./components/Login";
+import Login from "./components/login";
 import Dashboard from "./components/Dashboard";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
