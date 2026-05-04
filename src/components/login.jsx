047faae4addb0fce5c68aa63acba4b1807793f52
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-05-04 11:57:18 +0100

    Update login component

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -25,7 +25,7 @@ const Login = () => {
 
     try {
       const response = await fetch(
-        "http://localhost:5000/api/auth/login",
+        "https://muloanaptech.onrender.com/api/auth/login",
         {
           method: "POST",
           headers: {
