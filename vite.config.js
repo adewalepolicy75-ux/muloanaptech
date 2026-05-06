0bb1950cdfd9be956cad410195a1105da2fdd53d
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-05-06 11:11:58 +0100

    Fix vite.config.js assets path

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,14 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   build: {
-    assetsDir: '../assets',
-    outDir: 'dist'
+    outDir: 'dist',
+    assetsDir: 'assets',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name]-[hash].js',
+        chunkFileNames: 'assets/[name]-[hash].js',
+        assetFileNames: 'assets/[name]-[hash].[ext]'
+      }
+    }
   }
 })
