43e9186b982d7c95fc1e156071c76fd39995d7c1
Abdulkabir Adekunle <adewalepolicy75@gmail.com>
2026-05-08 11:52:24 +0100

    Clean up App.jsx routes - remove duplicates

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,12 @@ import Privacy from "./components/Privacy";
 import Terms from "./components/Terms";
 import Faq from "./components/Faq";
 import Home from "./pages/Home";
-import Loans from "./pages/Loans";
 import Signup from "./components/Signup";
-import Login from "./components/login";
+import Login from "./components/Login";
 import Dashboard from "./components/Dashboard";
-import LoanDetails from "./components/LoanDetails";
 import ApplyLoan from "./components/ApplyLoan";
+import LoanDetails from "./components/LoanDetails";
+import Loans from "./pages/Loans";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
@@ -32,10 +32,8 @@ function App() {
           <Route path="/login" element={<Login />} />
           <Route path="/dashboard" element={<Dashboard />} />
           <Route path="/apply" element={<ApplyLoan />} />
-<Route path="/loan/:id" element={<LoanDetails />} />
-          <Route path="/loans" element={<Loans />} /> 
-          <Route path="/loans" element={<LoanDetails/>} /> 
-           
+          <Route path="/loan/:id" element={<LoanDetails />} />
+          <Route path="/loans" element={<Loans />} />
         </Routes>
         <Footer />
       </div>
